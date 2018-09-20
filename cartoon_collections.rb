cd740049d883b98ad1a5e6e143707882f3513ed7
Mac Dowell Prosper <macdowell@noukod.com>
2018-09-20 17:19:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(arr)
   arr.map {|val| val.capitalize << "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  arr.any? {|val| val.size > 4}
 end
 
 def find_the_cheese# code an argument here
